01a8cc4dc7b5fd6a5f9a4d2d314e4a18513ab871
katafuchix <katafuchix@gmail.com>
2026-04-23 20:47:51 +0900

    Initial Commit

---
+++ SwiftUI_YahooImageSearch_TCA/SwiftUI_YahooImageSearch_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_YahooImageSearch_TCAApp.swift
+//  SwiftUI_YahooImageSearch_TCA
+//
+//  Created by cano on 2026/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_YahooImageSearch_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
